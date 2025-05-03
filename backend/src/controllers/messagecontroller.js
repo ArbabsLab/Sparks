d15fc7e2fe22ff6092be9ceba7a4468f533ffee7
Arbab <arbabhusain200@gmail.com>
2025-05-02 20:16:47 -0400

    Added getmessages

--- backend/src/controllers/messagecontroller.js
+++ backend/src/controllers/messagecontroller.js
@@ -1,12 +1,28 @@
-import User from "../models/usermodel";
+import User from "../models/usermodel.js";
+import Message from "../models/messagemodel.js";
 
 export const getSidebar = async (req, res) => {
     try {
-        const you = req.user._id;
-        const filteredUsers = await User.find({_id: {$ne:you}}).select("-password");
+        const me = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne:me}}).select("-password");
 
         res.status(200).json(filteredUsers);
     }catch(e){
         res.status(500).json({message: e})
+    }
+}
+
+export const getMessage = async (req, res) => {
+    try {
+        const me = req.user._id;
+        const {id:you} = req.params;
+
+        const messages = await Message.find({
+            $or:[ {sender:me, receiver:you}, {sender:you, receiver:me}]
+        })
+
+        res.status(200).json({messages});
+    }catch(e){
+        res.status(500).json({message:e})
     }
 }
--- backend/src/routes/message.js
+++ backend/src/routes/message.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { checkUser } from '../middleware/authmiddleware.js';
+import { getMessage, getSidebar } from '../controllers/messagecontroller.js';
 
 const router = express.Router();
 
+router.get("/users", checkUser, getSidebar);
+router.get("/:id", checkUser, getMessage)
 
 
 
