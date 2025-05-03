a0943cadc8e13bf344b7406588df307d4d1c941e
Arbab <arbabhusain200@gmail.com>
2025-05-03 12:24:59 -0400

    Added frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import Settings from './pages/Settings.jsx'
+import Profile from './pages/Profile.jsx'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-
+      App
       <Routes>
+      
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/settings" element={<Setting />} />
+        <Route path="/settings" element={<Settings />} />
         <Route path="/profile" element={<Profile />} />
+      
       </Routes>
     </div>
   )
